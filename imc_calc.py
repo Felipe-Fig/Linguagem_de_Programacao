e5f0e33420f5ce69b9231a06cb8c14984fdff278
detektivo <110146061+Felipe-Fig@users.noreply.github.com>
2022-09-06 18:31:52 -0300

    finalizados ifs

--- imc_calc.py
+++ imc_calc.py
@@ -20,3 +20,17 @@ except ValueError:
 altura_m = altura_cm / 100
 imc = peso / (altura_m*altura_m)
 #printando os outputs
+print(f"\nO seu IMC é {imc:.2f}. Agora vamos à interpretação deste valor...")
+time.sleep (2)
+# IMC entre 18,5 e 24,9 = Normal
+if 18.5 > imc > 24.9:
+    print("\nDe acordo com o parâmetro IMC seu peso está normal!\n")
+# IMC entre 25,0 e 29,9 = Sobrepeso
+elif 25 > imc > 29.9:
+    print("\nDe acordo com o parâmetro IMC você está em sobrepeso.\n")
+# IMC entre 30,0 e 39,9 = Obesidade
+elif 30 > imc > 39.9:
+    print("\nDe acordo com o parâmetro IMC você está obeso.\n")
+# IMC maior que 40,0 = Obesidade grave
+else:
+    print("\nDe acordo com o parâmetro IMC você está gravemente obeso.\n")
